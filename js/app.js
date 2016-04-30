3b2c2121adad4b6cec8b80ebd741af51f11b2266
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-30 17:16:50 +0530

    new updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-$(function (){
-	console.log("yep");
-})
+console.log('hey');
+angular.module('myApp', [])
+	.controller('fillContent', function($scope) {
+
+		var content = {
+			maleName: '(Name)',
+			job: '(Dirty Job)',
+			task: '(Task)',
+			celebrity: '(Celebrity)',
+			skill: '(Skill)',
+			adjective:'(Adjective)',
+			number: '(Number)'
+
+
+		};
+
+		$scope.content = content;
+
+});
