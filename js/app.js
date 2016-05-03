933193ed78a5b616ed71877aa922bcef4b9211db
Rakesh Patel <emailme@rakeshrpatel.com>
2016-05-03 08:22:42 +0530

    complete

--- js/app.js
+++ js/app.js
@@ -3,25 +3,37 @@ angular.module('myApp', [])
     .controller('fillContent', function($scope) {
 
         var content = {
-            maleName: '',
-            job: '',
-            task: '',
-            celebrity: '',
-            skill: '',
-            adjective: '',
-            number: ''
-
+        	maleName:"Joey"
         };
+            // maleName: '',
+            // job: '',
+            // task: '',
+            // celebrity: '',
+            // skill: '',
+            // adjective: '',
+            // number: ''
 
         $scope.content = content;
-
+        $scope.crazyScope = false;
         $scope.submit = function() {
-            if ($scope.myForm.$valid) {
-                return true;
-            } else {
-                return false;
-            }
+            $scope.crazyScope = $scope.myForm.$valid
+            // if ($scope.myForm.$valid) {
+            //     $scope.crazyScope=true;
+
+            // } else {
+            //     $scope.crazyScope=false;
+
+            // }
         }
+
+        // How to reset the form - fail one of the hide/show conditions.
+        $scope.startOver = function(){
+        	$scope.myForm.$submitted = false;
+        	$scope.crazyScope=false;
+        	$scope.content = {};
+        }
+
+
 
 
     });
