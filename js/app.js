326a0aeeefbed40834128df0054300605d5d93c8
Rakesh Patel <emailme@rakeshrpatel.com>
2016-05-24 14:59:25 -0400

    finished product

--- js/app.js
+++ js/app.js
@@ -1,29 +1,13 @@
-var app = angular.module('myApp', ['ngAnimate']);
-
-angular.module('myApp', [])
+angular.module('myApp', ['ngAnimate'])
     .controller('fillContent', function($scope) {
 
-        var content = {
-            maleName: 'Joey',
-            job: 'TashCan Washer',
-            task: 'Wash plates',
-            celebrity: 'Michelle Phieffer',
-            skill: 'Glue together yardsticks',
-            adjective: 'uberrific',
-            number: ''
-            };
+        var content = {};
 
         $scope.content = content;
         $scope.crazyScope = false;
         $scope.submit = function() {
             $scope.crazyScope = $scope.myForm.$valid
-            // if ($scope.myForm.$valid) {
-            //     $scope.crazyScope=true;
 
-            // } else {
-            //     $scope.crazyScope=false;
-
-            // }
         }
 
         // How to reset the form - fail one of the hide/show conditions.
